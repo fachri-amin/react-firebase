41f8ca747ceb15b44aa0c4f0fd5747570e4e78ff
fachri-amin <fachri.amin@gmail.com>
2021-02-26 21:15:17 +0700

    installing redux

--- src/containers/pages/App/index.js
+++ src/containers/pages/App/index.js
@@ -2,15 +2,19 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from '../Login';
 import Register from '../Register';
 import Dashboard from '../Dashboard';
+import { Provider } from 'react-redux';
+import store from '../../../config/redux';
 import './App.css';
 
 function App() {
   return (
-    <Router>
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/login" component={Login} />
-      <Route path="/" component={Register} />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <Route path="/dashboard" component={Dashboard} />
+        <Route path="/login" component={Login} />
+        <Route path="/" component={Register} />
+      </Router>
+    </Provider>
   );
 }
 
--- src/containers/pages/Register/index.js
+++ src/containers/pages/Register/index.js
@@ -1,15 +1,16 @@
 import { useState, useEffect } from 'react';
 import auth_firebase from '../../../config/auth';
+import { connect } from 'react-redux';
 import './Register.scss';
 
-const Register = () => {
+const Register = (props) => {
     const [email, setEmail] = useState(0)
     const [password, setPassword] = useState(0)
 
     return (
         <div className='auth-container'>
             <div className="auth-card">
-                <p className='auth-title'>Register</p>
+                <p className='auth-title'>Register {props.popup}</p>
                 <input className='input' onChange={e => setEmail(e.target.value)} type="email" id='email' placeholder='masukkan email' />
                 <input className='input' onChange={e => setPassword(e.target.value)} type="password" id='password' placeholder='masukkan password' />
                 <button className='btn' onClick={() => auth_firebase(email, password)}>Register</button>
@@ -18,4 +19,8 @@ const Register = () => {
     )
 }
 
-export default Register;
+const mapStateToProps = (state) => ({
+    popup: state.popup,
+})
+
+export default connect(mapStateToProps, null)(Register);
